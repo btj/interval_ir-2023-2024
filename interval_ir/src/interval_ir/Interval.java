e617e53f82aed0dc390c2067ce6646d3244c01c4
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-02-20 18:16:48 +0100

    Documentatie

--- interval_ir/src/interval_ir/Interval.java
+++ interval_ir/src/interval_ir/Interval.java
@@ -1,7 +1,19 @@
 package interval_ir;
 
+/**
+ * Elke instantie van deze klasse slaat een interval van de gehele as op.
+ * 
+ * @invar De bovengrens is niet kleiner dan de ondergrens.
+ *     | getOndergrens() <= getBovengrens()
+ * @invar De lengte is gelijk aan het verschil tussen de bovengrens en de ondergrens.
+ *     | getLengte() == getBovengrens() - getOndergrens()
+ */
 public class Interval {
 	
+	/**
+	 * @invar De bovengrens is niet kleiner dan de ondergrens.
+	 *     | ondergrens <= bovengrens
+	 */
 	private int ondergrens;
 	private int bovengrens;
 	
@@ -18,12 +30,32 @@ public class Interval {
 		return this.bovengrens - this.ondergrens;
 	}
 	
+	/**
+	 * Initialiseert dit object met de gegeven ondergrens en lengte.
+	 * 
+	 * @pre De gegeven lengte is niet negatief.
+	 *     | 0 <= initiëleLengte
+	 * @post De ondergrens is gelijk aan de gegeven ondergrens.
+	 *     | getOndergrens() == initiëleOndergrens
+	 * @post De lengte is gelijk aan de gegeven lengte.
+	 *     | getLengte() == initiëleLengte
+	 */
 	public Interval(int initiëleOndergrens, int initiëleLengte) {
 		this.ondergrens = initiëleOndergrens;
 		this.bovengrens = initiëleOndergrens + initiëleLengte;
 	}
 	
-	// Setters (ook mutatoren genoemd)
+	/**
+	 * Stelt de ondergrens in op de gegeven waarde.
+	 * 
+	 * @pre De gegeven ondergrens is niet groter dan de huidige bovengrens.
+	 *     | nieuweOndergrens <= getBovengrens()
+	 * @mutates | this
+	 * @post De ondergrens is gelijk aan de gegeven waarde.
+	 *     | getOndergrens() == nieuweOndergrens
+	 * @post De bovengrens blijft ongewijzigd
+	 *     | getBovengrens() == old(getBovengrens()) 
+	 */
 	public void setOndergrens(int nieuweOndergrens) {
 		this.ondergrens = nieuweOndergrens;
 	}
